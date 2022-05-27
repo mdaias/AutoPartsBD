fcafb98c62f245cbe276c21279f9f18efabdc0ce
mdaias <mdaias29@gmail.com>
2022-05-27 23:04:04 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+        </Routes>
+
         <h1 className='text-3xl font-bold underline text-violet-500'>welcome to AutoPartsBd</h1>
     </div>
   );
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../../Shared/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
-            <h1>its home</h1>
+            <Navbar></Navbar>
         </div>
     );
 };
