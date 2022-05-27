999c6a20ecba68c98fbd2be964a7cf016217cdd7
mdaias <mdaias29@gmail.com>
2022-05-27 21:08:39 +0600

    install package

---
+++ src/pages/Authentication/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>please Register</h1>
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/pages/Home/BusinessSummary/BusinessSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BusinessSummary = () => {
+    return (
+        <div>
+            <h1>BusinessSummary</h1>
+        </div>
+    );
+};
+
+export default BusinessSummary;
